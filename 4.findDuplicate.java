c161cabce172fe8b07af3422e6cd3dc29a9f8c14
Jensenczx <jensenczx@gmail.com>
2016-11-22 11:12:58 +0800

    crack the findDuplicate problem by binary search

---
+++ 4.findDuplicate.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if(nums == null || nums.length == 1)
+        	return 0;
+       
+    }
+
+    public int findDuplicate(int[] nums) {
+    	if(nums == null || nums.length = 0)
+    		return 0;
+    	int mid = 0, low = 1, high = nums.length - 1;
+    	while(low < high) {
+    		mid = (low + high)/2;
+    		int count = 0;
+    		for (int i = 0; i < nums.length; i++) {
+    			if(nums[i] <= mid)
+    				count++;
+    		}
+    		if(count > mid) {
+    			high = mid;
+    		} else {
+    			low = mid + 1;
+    		}
+    	}
+    	return low;
+    }
+}
